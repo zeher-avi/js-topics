bc69e763b67c0e5feccbe44c215560e3c00757bc
avi <contact.avi22@gmail.com>
2023-11-18 17:41:43 +0530

    Added 02

--- tutorial/02_datatypes.js
+++ tutorial/02_datatypes.js
@@ -1,1 +1,22 @@
 'use strict'; //treats javascript as the newer version
+
+//*Data types in JavaScript
+
+/*
+number = 2 to the power 53 
+bigint = for large numbers
+string
+boolean 
+null = standalone value
+undefined
+symbol = unique
+Object
+*/
+
+let number = 23;
+let string = 'Pablo';
+let boolean = false;
+let standalone = null;
+let udf = '';
+let symbol = Symbol('foo');
+let object = {};
