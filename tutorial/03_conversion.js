d1a4fd8312d69543300a65c0807d604e2f4b5f7c
avi <contact.avi22@gmail.com>
2023-11-20 15:49:49 +0530

    Strings

---
+++ tutorial/03_conversion.js
@@ -0,0 +1,10 @@
+let age = '30';
+let boolean = 1;
+
+//conversion of string to number
+const ageInNumbers = Number(age);
+console.log(typeof ageInNumbers);
+
+//conversion of number in boolean
+const booleanIsTrue = Boolean(boolean);
+console.log(booleanIsTrue); //* shows true cuz 1 is true
---
+++ tutorial/04_string.js
@@ -0,0 +1,23 @@
+let name = 'abhinav';
+let repoCount = 3;
+
+//? modern syntax of printing 2 variables in a single line
+// console.log(`${name} have ${repoCount} repos`);
+
+//? will print the length of the name variable
+// console.log(name.length);
+
+//? will turn it into upperCase
+// console.log(name.toUpperCase());
+
+//? will slice it into desired characters
+// const newName = name.slice(0, 3);
+// console.log(newName);
+
+//? will remove the empty spaces
+// const newStringOne = '                    avi              ';
+// console.log(newStringOne.trim());
+
+//? will replace the % with -
+// const url = 'www.google.com/archive%20blogs';
+// console.log(url.replace('%20', '-'));
